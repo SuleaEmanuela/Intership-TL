bf0052c303a05225ff6893250c2e2849307f86ad
GaleaStefan <stefanandreig@gmail.com>
2021-07-29 12:51:31 +0300

    Resolved Ex4.

--- Ex1/Ex1/GreetingService.cs
+++ Ex1/Ex1/GreetingService.cs
@@ -1,21 +1,35 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Ex1
 {
     public class GreetingService
     {
 #nullable enable
-        public string Greet(string? name)
+        private string ParseName(string? name, out bool isShouting)
         {
-
-            bool isShouting = (name?.All(ch => char.IsUpper(ch))) is not null and true;
+            isShouting = (name?.All(ch => char.IsUpper(ch))) is not null and true;
             var replacement = (name is null) ? "my friend" : name;
-            var greetString = $"Hello, {replacement} .";
-            if (isShouting)
+
+            return replacement;
+        }
+
+        public string Greet(params string[] names)
+        {
+            var resultString = "Hello";
+            var endOfString = " .";
+
+            for(int index = 0; index < names.Length - 1; index++)
             {
-                greetString = greetString.ToUpper();
+                resultString += ", " + ParseName(names[index], out bool _);
             }
-            return greetString;
+
+            var lastSeparator = (names.Length == 1) ? ", " : " and ";
+            resultString += lastSeparator + ParseName(names[^1], out bool isShouting) + endOfString;
+
+            if (isShouting) resultString = resultString.ToUpper();
+
+            return resultString;
         }
 #nullable disable
     }
--- Ex1/NUTestsEx1/GreetingServiceTest.cs
+++ Ex1/NUTestsEx1/GreetingServiceTest.cs
@@ -1,12 +1,15 @@
 using NUnit.Framework;
 using Ex1;
+using System.Collections.Generic;
+using System;
+
 namespace NUTestsEx1
 {
     public class GreetingServiceTest
     {
         private GreetingService Service;
         [OneTimeSetUp]
-        public void  InitializeService()
+        public void InitializeService()
         {
             Service = new();
         }
@@ -19,14 +22,14 @@ namespace NUTestsEx1
             var expectedResult = $"Hello, {name} .";
             var actualResult = Service.Greet(name);
             Assert.That(actualResult, Is.EqualTo(expectedResult));
-            
+
         }
 
         [Test]
         public void ShouldHandleNullNames()
         {
             var expectedResult = $"Hello, my friend .";
-            var actualResult = Service.Greet(null);
+            var actualResult = Service.Greet((string)null);
 
             Assert.That(actualResult, Is.EqualTo(expectedResult));
         }
@@ -38,6 +41,14 @@ namespace NUTestsEx1
             var exepctedResult = $"HELLO, {name} .";
             var actualResult = Service.Greet(name);
             Assert.That(actualResult, Is.EqualTo(exepctedResult));
+        }
+
+        [Test]
+        [TestCase("Stefan", "Emma")]
+        public void ShouldHandleTwoNames(params string[] names)
+        {
+            string expectedResult = $"Hello, {names[0]} and {names[1]} .";
+            string actualResult = Service.Greet(names);
         }
     }
 }
